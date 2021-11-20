559db1851fae12b60f7320bfb54a192f4e5045ef
Oleg Sovetnik <sovetnik@oblaka.biz>
2021-11-20 18:29:56 +0300

    Create app

---
+++ lib/hello_elixir.ex
@@ -0,0 +1,18 @@
+defmodule HelloElixir do
+  @moduledoc """
+  Documentation for `HelloElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HelloElixir.hello()
+      "Heil Elixir!"
+
+  """
+  def hello do
+    "Heil Elixir!"
+  end
+end
